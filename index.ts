19479111374e6bc151be3a4005bd5348ddced53d
gabrielregis3 <txiloo3@gmail.com>
2024-10-17 17:32:33 -0300

    add function to save tasks at localstorage

--- index.ts
+++ index.ts
@@ -11,6 +11,8 @@ const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.querySelector<HTMLFormElement>("#new-task-form")
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
+const tasks: Task[] = []
+
 form?.addEventListener("submit", e => {
     e.preventDefault() //Prevents the page to reload when sending a form
 
@@ -22,19 +24,29 @@ form?.addEventListener("submit", e => {
         completed: false,
         createdAt: new Date()
     }
+    tasks.push(newTask)
 
     addListItem(newTask)
+    input.value = ""
 })
 
 function addListItem(task: Task) {
     const item = document.createElement("li")
     const label = document.createElement("label")
     const checkbox = document.createElement("input")
-
+    
+    checkbox.addEventListener("change", () => {
+        task.completed = checkbox.checked
+        saveTasks()
+    })
     checkbox.type = "checkbox"
-
+    checkbox.checked = task.completed
     label.append(checkbox, task.title)
 
     item.append(label)
     list?.append(item)
+}
+
+function saveTasks() {
+    localStorage.setItem("TASKS", JSON.stringify(tasks))
 }
