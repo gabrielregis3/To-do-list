0e089caafdccfbe62754db3c8e7239924ed5d503
gabrielregis3 <txiloo3@gmail.com>
2024-10-17 13:14:18 -0300

    add types and task var

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
-const list = document.querySelector("#list")
-const form = document.querySelector("#new-task-form")
-const input = document.querySelector("#new-task-title")
+import { v4 as uuidV4 } from "uuid"
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector<HTMLFormElement>("#new-task-form")
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+form?.addEventListener("submit", e => {
+    e.preventDefault() //Prevents the page to reload when sending a form
+
+    if (input?.value == "" || input?.value == null) return //Input? means that if exists, give the value, if doesn't, return undefined
+
+    const task = {
+        id: uuidV4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date()
+    }
+})
