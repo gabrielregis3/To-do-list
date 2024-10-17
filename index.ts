9d73d014e587090345a9a216ba7b54fe1f5d519f
gabrielregis3 <txiloo3@gmail.com>
2024-10-17 15:03:43 -0300

    add function to create a task

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 import { v4 as uuidV4 } from "uuid"
 
+type Task = { 
+    id: string, 
+    title: string, 
+    completed: boolean, 
+    createdAt: Date
+}
+
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.querySelector<HTMLFormElement>("#new-task-form")
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
@@ -9,10 +16,25 @@ form?.addEventListener("submit", e => {
 
     if (input?.value == "" || input?.value == null) return //Input? means that if exists, give the value, if doesn't, return undefined
 
-    const task = {
+    const newTask: Task = {
         id: uuidV4(),
         title: input.value,
         completed: false,
         createdAt: new Date()
     }
+
+    addListItem(newTask)
 })
+
+function addListItem(task: Task) {
+    const item = document.createElement("li")
+    const label = document.createElement("label")
+    const checkbox = document.createElement("input")
+
+    checkbox.type = "checkbox"
+
+    label.append(checkbox, task.title)
+
+    item.append(label)
+    list?.append(item)
+}
